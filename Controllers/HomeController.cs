527c5fdb056e4b04baa7d39e9ee93be2c2646ea0
Mateusz <allegro6@gazeta.pl>
2018-11-01 14:08:35 +0100

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Stoper3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ResetLaps();
+            return View();
+        }
+
+        public static List<string> times;
+        public JsonResult Lap(string obj)
+        {
+            ViewBag.YourHTML = "obj;";
+           
+            times.Add(obj);
+            if (times.Count>5)
+            {
+                times.Remove(times[0].ToString());
+            }
+
+            List<string> timesTemp = new List<string>();
+            for (int i=1;i<=times.Count;i++)
+            {
+                timesTemp.Add(times[times.Count - i]);
+            }
+            
+            return Json(timesTemp);
+        }
+
+       public void ResetLaps()
+        {
+            times = new List<string>();
+        }
+
+    }
+}
